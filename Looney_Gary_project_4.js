0370367c87d38ad4d1528ff3486a42a0826b2001
Gary W. H. Looney <hunterlooney@fullsail.edu>
2013-03-26 15:05:21 -0400

    Set up 2

--- Looney_Gary_project_4.js
+++ Looney_Gary_project_4.js
@@ -12,16 +12,37 @@
 
 
 
+//String
+//1)Does a string follow a 123-456-7890 pattern like a 
+//  phone number?
+
+//2)Does a string follow an aaa@bbb.ccc pattern like 
+//  an email address?
+
+//3)Is the string a URL? (Does it start with http: or 
+//  https:?)
+
+//4)Title-case a string (split into words, then 
+//  uppercase the first letter of each word)
+
+//5)Given a string that is a list of things separated 
+//  by a given string, as well as another string separator,
+//  return a string with the first separator changed to 
+//  the second: "a,b,c" + "," + "/" → "a/b/c".
 
 
+//Number
+//1)Format a number to use a specific number of decimal 
+//places, as for money: 2.1 → 2.10
 
+//2)Fuzzy-match a number: is the number above or below a 
+//number within a certain percent?
 
+//3)Find the number of hours or days difference between 
+//two dates.
 
-
-
-
-
-
+//4)Given a string version of a number such as "42", 
+//return the value as an actual Number, such as 42.
 
 
 //Array
@@ -35,3 +56,36 @@
 //return the array sorted by the value of that key 
 //in each of the objects: 
 //"a" + [{a:2},{a:3},{a:1}] → [{a:1},{a:2},{a:3}].
+
+
+
+//Make-up
+/*
+// You may use this assignment to make up some of what you 
+// lost on points for the first three assignments:
+
+// Implement all twelve functions above. At least ten of 
+// them should work correctly, but any that do not work 
+// should at least have a solid effort in them. If you 
+// don't get at least this far, none of the rest of this 
+// list applies.
+
+// Comment the sections of your code that show that you 
+// know how to do whatever you lost points for on a 
+// previous assignment. If you don't have an explicit 
+// comment you won't get credit. For example, if you lost 
+// points for not including a nested loop in 
+// Deliverable 3, include a comment that says something 
+// like:
+// 1
+// MAKEUP: nested loops, deliverable 3
+// For each fixed instance you may earn up to ¾ or 75% 
+// of the points you lost for that item. So if you lost 
+// 8 points for an item on the previous assignment, you 
+// can get 6 of them back.
+// Your fixed code must work! There is no partial credit 
+// per item. In the above example, it's 6 points or nothing.
+// Yes, it's going to be a whole lot of work to get the 
+// points back. But it's an opportunity to make up 75% of 
+// what you lost.
+*/
