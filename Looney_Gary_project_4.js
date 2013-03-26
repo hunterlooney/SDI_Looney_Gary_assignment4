ea74db4638501b9eeb7fddd4827124c21660f16d
Gary W. H. Looney <hunterlooney@fullsail.edu>
2013-03-26 14:56:53 -0400

    Set up

---
+++ Looney_Gary_project_4.js
@@ -0,0 +1,37 @@
+//Gary W. Hunter Looney
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Array
+//1)Find the smallest value in an array that is greater 
+//than a given number
+
+//2)Find the total value of just the numbers in an array, 
+//even if some of the items are not numbers.
+
+//3)Given an array of objects and the name of a key, 
+//return the array sorted by the value of that key 
+//in each of the objects: 
+//"a" + [{a:2},{a:3},{a:1}] → [{a:1},{a:2},{a:3}].
